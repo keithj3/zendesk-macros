51f5baf7daa22c46bed57720099be8500a4e1157
keithj3 <11054821+keithj3@users.noreply.github.com>
2019-01-31 22:49:58 -0600

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,8 @@
+''' credentials for zendesk '''
+
+ZD_KEY = #'ZD token string'
+ZD_BASE_URL = # 'https://COMPANY.zendesk.com/api/v2/' 
+ZD_EMAIL = #'admin@company.com'
+
+agent_email = # 'agent@company.com' #Agent making macro change request
+macro_id = #Macro Int
